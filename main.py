26513d41f7f2c75240812e84f5321294bb176bc8
Александр <musecollaboration.dev@gmail.com>
2026-07-18 16:20:45 +0300

    Добавление двух новых маршрутов: /func1 и /func2 для обработки запросов

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/')
-async def root():
-    return {'message': 'OK'}
+@app.get('/func1')
+async def func1():
+    return {'func1 message': 'OK'}
+
+
+@app.get('/func2')
+async def func2():
+    return {'func2 message': 'OK'}
